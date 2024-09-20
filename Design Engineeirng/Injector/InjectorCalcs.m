4ac3da8a861af7ce7de48990808533e6baba9c10
Cole <111032193+ColioDa@users.noreply.github.com>
2024-09-20 15:13:54 -0400

    updated injectorcalcs

--- Design Engineeirng/Injector/InjectorCalcs.m
+++ Design Engineeirng/Injector/InjectorCalcs.m
@@ -1,13 +1,15 @@
 clear, clc, close all
 %Plug in values
-rho = 49.01; % lb/ft^3 density 
-g = 32.2; %ft/s^2 accel. due to gravity
-dP = 350 * 144; %psf pressure drop across injector plate
-n = 7; %number of holes
-w = 0.1177; %lb/s flowrate
-v = 100; %ft/s
-Cd = v / sqrt(2*g*(dP/rho)); %should be between 0.5 and 0.7
-A = ((w/(Cd*sqrt(2*g*dP*rho)))*144)*6.4516; %cm^2
-D = sqrt((4*A*pi)/n);
-disp("The coefficient of discharge is " + Cd)
-disp("The diameter of each hole is " + D + " cm")
+Cd = 0.6;
+g = 9.81;
+rho = 786;
+n = 4;
+dP = 300000; %MPa
+mdot = 0.0534;
+wdot = mdot * g
+
+v = Cd*sqrt(2*g*(dP/rho))
+A = wdot/(Cd*sqrt(2*g*rho*dP))*1000000
+
+D = sqrt(((4*A)/pi)/n)
+
